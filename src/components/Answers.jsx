3572db780142bbae91ede0bc0308aac873b02723
Giovanni Crudele <crudelegio@gmail.com>
2024-02-21 16:05:58 +0100

    added the "shuffle" for the answers by using the sort method and avoiding Fisher/Yates method

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,13 +1,28 @@
-import QUESTIONS from "../questions.js";
 // TODO: you need to import here 1 hooks
 
-export default function Answers({ answers, showNextQuestion }) {
+export default function Answers({
+  answers,
+  showNextQuestion,
+  handleAnswerClick,
+}) {
+  //funzione per mescolare le risposte (Fisher-Yates puppami la fava)
+  const shuffleAnswers = () => {
+    return answers.sort(() => Math.random() - 0.5);
+  };
+
+  //salviamoci la nostra funzioncina in una variabile
+  const shuffledAnswers = shuffleAnswers();
+
   return (
     <ul id="answers">
       {/* key nel li che prende index */}
-      {answers.map((answer, index) => (
+      {shuffledAnswers.map((answer, index) => (
         <li key={index} className="answer">
-          <button className="" onClick={showNextQuestion}>
+          <button
+            className=""
+            onClick={showNextQuestion}
+            onAnswerClick={handleAnswerClick}
+          >
             {answer.text}
           </button>
         </li>
