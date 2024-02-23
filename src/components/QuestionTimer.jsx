41b57c41630f4e9b73b8fef52691ca3c17efea53
Giovanni Crudele <crudelegio@gmail.com>
2024-02-23 15:28:06 +0100

    improved smoothness of progress bar

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function QuestionTimer({ onTimeGong }) {
-  const [timer, setTimer] = useState(20);
+  const [timer, setTimer] = useState(200);
 
   useEffect(() => {
     //se il tempo arriva a 0, "suono il gong" e mi sbatte a Summary
@@ -13,12 +13,12 @@ export default function QuestionTimer({ onTimeGong }) {
     //scaliamo secondo per secondo
     const timerID = setTimeout(() => {
       setTimer(timer - 1);
-    }, 1000);
+    }, 100);
 
     return () => clearTimeout(timerID);
 
     //nelle dipendenze passiamo i valori da aggiornare ad ogni rendering
   }, [timer, onTimeGong]);
 
-  return <progress id="question-time" max={20} value={timer} className="" />;
+  return <progress id="question-time" max={200} value={timer} className="" />;
 }
